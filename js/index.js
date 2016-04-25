475f0c1ed55b150b6eb097ce218130fefa09edde
Vachagan Aslanyan <vachagan.aslanyan.1992@hotmail.com>
2016-04-26 00:44:46 +0400

    responsive

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    var person = prompt("Its you  Aghas???(Yes or No)", "Yes");
+    if(person == "Yes"){
+        window.open("contact-form/Aghas/Aghas.html");
+    }
+})

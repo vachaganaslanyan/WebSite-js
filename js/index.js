ecf3259c2df14c07107adc25156e7512edaeeb3f
Vachagan Aslanyan <vachagan.aslanyan.1992@hotmail.com>
2016-04-26 04:08:26 +0400

    add contact

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var person = prompt("Its you  Aghas???(Yes or No)", "Yes");
-    if(person == "Yes"){
+    if (person == "Yes") {
         window.open("contact-form/Aghas/Aghas.html");
     }
+
+    $("#contactf").click(function () {
+        $(".back, #form").fadeIn();
+        center();
+    });
+
+    $(".back,.close").click(function () {
+        $(".back,#form").fadeOut();
+    });
+
+    function center() {
+        var form = $("#form");
+        var width = $(window).width() / 2 - (form.width() + 30) / 2;
+        var heigth = $(window).height() / 2 - (form.height() + 40) / 2;
+        form.css({
+            "left": width,
+            "top": heigth
+        });
+        return false;
+    }
+
+
 })
+
+

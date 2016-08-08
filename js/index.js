0f413e186cdc932185577551dd52942878950ab1
Vachagan Aslanyan <vachagan.aslanyan.1992@hotmail.com>
2016-08-08 16:04:23 +0400

    first

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-    var person = prompt("Its you  Aghas???(Yes or No)", "Yes");
-    if (person == "Yes") {
-    var conf = confirm("Answer this question");
-        if(conf == true){
-            window.open("contact-form/Aghas/Aghas.html");
-        }
-    }
 
     $("#contactf").click(function () {
         $(".back, #form").fadeIn();
